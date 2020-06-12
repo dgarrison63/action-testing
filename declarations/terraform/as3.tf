73227e7ce558be881358bf4deb74184c64f28541
David Garrison <david@Davids-MacBook-Pro.local>
2020-06-12 01:59:59 -0700

    update

--- declarations/terraform/as3.tf
+++ declarations/terraform/as3.tf
@@ -3,7 +3,7 @@ Copyright 2019 F5 Networks Inc.
 This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
 If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
- 
+
 /*
 terraform {
   required_providers {
@@ -27,7 +27,16 @@ resource "bigip_as3" "as3_example" {
  #config_name = "myconfig"
 }
 */
-
+/*
 output "file_list" {
   value = var.file_list
+}
+*/
+
+resource "null_resource" "example2" {
+  for_each = var.file_list
+  provisioner "local-exec" {
+    command = "Get-Date"
+    interpreter = ["PowerShell", "-Command"]
+  }
 }
