274d74845034745bad33a18f394e18ad6d257959
David Garrison <d.garrison@f5.com>
2020-06-18 12:19:57 -0700

    u[dated

--- declarations/terraform/as3.tf
+++ declarations/terraform/as3.tf
@@ -4,13 +4,12 @@ This Source Code Form is subject to the terms of the Mozilla Public License, v. 
 If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
 
-/*
+
 terraform {
   required_providers {
     terraform-providers/bigip = "~> 1.2.0"
   }
 }
-*/
 
 provider "bigip" {
   address = "https://54.144.87.71:8443"
